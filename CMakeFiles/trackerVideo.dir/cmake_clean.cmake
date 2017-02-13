d1783a3ac7fd1ef8a3755d0103a5c5341e8d2375
HugoStrange <hugolev@free.fr>
2017-02-13 19:23:22 +0100

    Fisrt commit

---
+++ CMakeFiles/trackerVideo.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/trackerVideo.dir/trackerVideo.cpp.o"
+  "trackerVideo.pdb"
+  "trackerVideo"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/trackerVideo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
